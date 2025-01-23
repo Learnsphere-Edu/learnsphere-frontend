254e18edea54abfbb6606499a31a2cf94edfe4ee
Peter Omotosho <omotoshopeter737@gmail.com>
2025-01-23 03:22:07 +0100

    update info details in the mapped InfoBoxes

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -12,8 +12,8 @@ export const InfoDetailsCurveRight:Info[] = [
     },
     {
         id: 3,
-        content: 'We make learning Nigerian languages fun and easy for kids, so they can connect with their roots and feel proud of their heritage.',
-        title: 'Bringing Nigerian Languages to Life',
+        content: 'Learn anytime, anywhere on any device, with tools that adapt to each child’s pace and progress.',
+        title: 'Flexible and Accessible',
         infoBg: 'bg-[#FF7A00]',
         textColor: 'text-[#ffffff]',
         titleColor: 'text-[#FF7A00]'
@@ -24,8 +24,8 @@ export const InfoDetailsCurveRight:Info[] = [
 export const InfoDetailsCurveLeft:Info[] = [
     {
         id: 1,
-        content: 'We make learning Nigerian languages fun and easy for kids, so they can connect with their roots and feel proud of their heritage.',
-        title: 'Bringing Nigerian Languages to Life',
+        content: 'Learnsphere combines fun games, visuals, and activities tailored to kids for an enjoyable learning experience. We keep kids excited about learning every step of the way.',
+        title: 'Engaging, Fun and Kid-Friendly',
         infoBg: 'bg-[#FF7A00]',
         textColor: 'text-[#ffffff]',
         titleColor: 'text-[#FF7A00]'
@@ -33,8 +33,8 @@ export const InfoDetailsCurveLeft:Info[] = [
     },
     {
         id: 2,
-        content: 'We make learning Nigerian languages fun and easy for kids, so they can connect with their roots and feel proud of their heritage.',
-        title: 'Bringing Nigerian Languages to Life',
+        content: 'Kids don’t just learn a language—they discover the stories, traditions, and values that make our cultures special.',
+        title: 'More Than Just Words',
         infoBg: 'bg-[#5B00FF]',
         textColor: 'text-[#ffffff]',
         titleColor: 'text-[#5B00FF]'
